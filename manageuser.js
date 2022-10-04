692bc519b13619db4ab765b93078a4f0dffe53ce
DaYlAmYaT <lam20034@byui.edu>
2022-10-04 13:46:04 -0600

    set for one time password

--- manageuser.js
+++ manageuser.js
@@ -9,12 +9,17 @@ function setphonenumber(){
     phonenumber = $("#phonenumber").val();
 }
 
-function setuseronetimepassword(){
+const sendText=()=>{
+    $.ajax({
+        type: "POST",
+        url: 'https://dev.stedi.me/twofactorlogin/'+phonenumber,
+        contentType: "application/text",
+        dataType: "text"
+    });
+}
+function setonetimepassword(){
     onetimepassword = $("#onetimepassword").val();
-    var valid=passwordRegEx.exec(password);
-    // if (!valid){
-    //     alert('Must be 6 digits, upper, lower, number, and symbol');
-    // }
+    
 }
 
 function setverifypassword(){
@@ -47,12 +52,12 @@ function checkexpiredtoken(token){
 }
 
 function userlogin(){
-    setuserpassword();
-    setusername();
+    setonetimepassword();
+    setphonenumber();
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/login',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin',
+        data: JSON.stringify({phoneNumber:phonenumber, oneTimePassword:onetimepassword}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
